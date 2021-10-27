8c59bb17f4b19db6f37882a516415868b1c21723
Fikcup <rhys.n.wright@gmail.com>
2021-10-26 19:30:44 -0700

    feat : table associations

--- models/index.js
+++ models/index.js
@@ -3,4 +3,9 @@ const Event = require('./Events');
 const EventType = require('./EventTypes');
 const Reservation = require('./Reservation');
 
+Customer.hasMany(Reservation, {foreignKey: { allowNull: false }});
+Event.hasMany(Reservation, {foreignKey: { allowNull: false }});
+Event.hasOne(EventType, {foreignKey: { allowNull: false }});
+Event.hasOne(Customer, {foreignKey: { allowNull: false }});
+
 module.exports = { Customer, Event, EventType, Reservation };
