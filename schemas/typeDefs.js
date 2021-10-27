ed6ae7141cbd3e378128f152abd7051bb2bd59a9
Fikcup <rhys.n.wright@gmail.com>
2021-10-27 10:44:08 -0700

    feat : typeDefs

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -1,6 +1,30 @@
 const { gql } = require('graphql');
 
 const typeDefs = gql`
+    type Customer {
+        _id: ID!
+        name: String!
+        phone: String!
+        email: String!
+        address: String!
+    }
+
+    type Event {
+        _id: ID!
+        name: String!
+    }
+
+    type EventType {
+        _id: ID!
+        name: String!
+        description: String!
+    }
+
+    type Reservation {
+        _id: ID!
+        eventDate: String!
+        headCount: Int!
+    }
 `;
 
 module.exports = typeDefs;
