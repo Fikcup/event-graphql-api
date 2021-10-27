3482912a533a9acf13402ecc11200cb0c3c751ef
Fikcup <rhys.n.wright@gmail.com>
2021-10-26 19:22:20 -0700

    feat : event type model

--- models/EventTypes.js
+++ models/EventTypes.js
@@ -5,7 +5,28 @@ class EventType extends Model {}
 
 EventType.init(
     {
-
+        uuid: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV1,
+            primaryKey: true,
+            allowNull: false,
+            unique: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true,
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            }
+        }
     },
     {
         sequelize,
