bff57b3e31b260f779b75c5e6ff4d9aae9a6c239
Fikcup <rhys.n.wright@gmail.com>
2021-10-26 19:26:35 -0700

    feat : reservation model

--- models/Reservation.js
+++ models/Reservation.js
@@ -5,7 +5,24 @@ class Reservation extends Model {}
 
 Reservation.init(
     {
-
+        uuid: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV1,
+            primaryKey: true,
+            allowNull: false,
+            unique: true,
+        },
+        eventDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                isDate: true
+            }
+        },
+        headCount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
     },
     {
         sequelize,
