785bf6795f17e9b813d9b0d187de0caf09c28cc3
Fikcup <rhys.n.wright@gmail.com>
2021-10-26 18:16:42 -0700

    feat : running server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json);
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+});
