3b8db551e2e8c700b536b4b5b256a864ea61f540
Fikcup <rhys.n.wright@gmail.com>
2021-10-26 18:18:41 -0700

    feat : database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,8 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App is listening on port ${PORT}`);
+    });
 });
