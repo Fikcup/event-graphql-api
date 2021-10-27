3b6be581d397e0b14f7ee407708d6103932cf4f9
Fikcup <rhys.n.wright@gmail.com>
2021-10-26 19:43:14 -0700

    fix : middleware correction

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
