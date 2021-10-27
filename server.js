3fe2c60e35d12c0078441c98d9ebc285788b1671
Fikcup <rhys.n.wright@gmail.com>
2021-10-27 10:28:35 -0700

    feat : temporary models import to populate tables with sequelize

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const models = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
