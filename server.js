ce899b2a66f7870e85fc7cdeb83a1bf01adf551d
Fikcup <rhys.n.wright@gmail.com>
2021-10-30 12:07:31 -0700

    feat : graphql apollo server

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const http = require('http');
+
 const models = require('./models');
+const { typeDefs, resolvers } = require('./schemas');
+const { graphql } = require('graphql');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const routes = require('./routes');
+async function startApolloServer(typeDefs, resolvers) {
+    const httpServer = http.createServer(app);
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+
+    await server.start();
+
+    server.applyMiddleware({ app });
+
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 
-app.use(routes);
-
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.log(`App is listening on port ${PORT}`);
-    });
+        startApolloServer(typeDefs, resolvers);
+    })
 });
