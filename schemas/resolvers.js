b4339334e4c318e44d7e3aa1ec04b5beb17ea8e4
Fikcup <rhys.n.wright@gmail.com>
2021-10-31 10:20:17 -0700

    feat : get all customers

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,7 +1,13 @@
-const { Customer, Event, EventType, Reservation } = require('../models');
+const { Customer, Event, EventType, Reservation } = require('../models/index');
 
 const resolvers = {
+    Query: {
+        async getAllCustomers() {
+            const customerData = await Customer.findAll()
 
+            return customerData;
+        },
+    },
 };
 
 module.exports = resolvers;
