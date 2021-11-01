c33e296bf4c9c30bc5cfa6685629afddfd0f5265
Fikcup <rhys.n.wright@gmail.com>
2021-11-01 09:45:25 -0700

    fix : table associations now allow for customer to be created

--- models/index.js
+++ models/index.js
@@ -3,9 +3,8 @@ const Event = require('./Event');
 const EventType = require('./EventType');
 const Reservation = require('./Reservation');
 
-Customer.hasMany(Reservation, {foreignKey: { allowNull: false }});
+Customer.hasMany(Reservation, {foreignKey: { allowNull: true }, onDelete: 'CASCADE'});
 Event.hasMany(Reservation, {foreignKey: { allowNull: false }});
 Event.hasOne(EventType, {foreignKey: { allowNull: false }});
-Event.hasOne(Customer, {foreignKey: { allowNull: false }});
 
 module.exports = { Customer, Event, EventType, Reservation };
