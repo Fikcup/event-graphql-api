a746dfc367b0a87434a5c3b95bbdcd83e45bb3e5
Fikcup <rhys.n.wright@gmail.com>
2021-11-03 22:09:47 -0700

    docs : table association next stepss

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Event = require('./Event');
 const EventType = require('./EventType');
 const Reservation = require('./Reservation');
 
+// TODO: fix table associations or queries to allow for reservations to be created
 Customer.hasMany(Reservation, {foreignKey: { allowNull: true }, onDelete: 'CASCADE'});
 Event.hasMany(Reservation, {foreignKey: { allowNull: false }});
 Event.hasOne(EventType, {foreignKey: { allowNull: false }});
